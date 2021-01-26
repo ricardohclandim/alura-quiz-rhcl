6033987a10c9ec8dc4f497eeccd6b4bcc7c73b90
ricardohclandim <rick.landan@gmail.com>
2021-01-26 20:32:35 -0300

    segunda aula

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
-import QuizLogo from '../src/components/QuizLogo'
-import Widget from '../src/components/widget'
+import React from 'react';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
+import db from '../db.json';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+import Widget from '../src/components/widget';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -19,6 +21,9 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
@@ -28,7 +33,22 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={function (event) {
+              event.preventDefault();
+
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={function (event) {
+                  setName(event.target.value);
+                }}
+                placeholder="Infome o seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
@@ -37,9 +57,9 @@ export default function Home() {
             <p> lorem ipsum dolor sit amet..</p>
           </Widget.Content>
         </Widget>
-      <Footer />
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto"/>
+      <GitHubCorner projectUrl="https://github.com/ricardohclandim/alura-quiz-rhcl" />
     </QuizBackground>
-  )
+  );
 }
