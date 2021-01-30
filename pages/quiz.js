a4ff12586c7cd281fbb24f97792f206910b634e2
ricardohclandim <rick.landan@gmail.com>
2021-01-29 21:54:53 -0300

    commit aula 4

--- pages/quiz.js
+++ pages/quiz.js
@@ -2,14 +2,48 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
+import AlternativesForm from '../src/components/AlternativesForm';
 import db from '../db.json';
 import Button from '../src/components/Button';
-import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizContainer from '../src/components/QuizContainer';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizLogo from '../src/components/QuizLogo';
 import Widget from '../src/components/widget';
+
+function ResultWidget({ results }) {
+  return (
+    <Widget>
+      <Widget.Header>
+        Tela de resultados
+      </Widget.Header>
+
+      <Widget.Content>
+        <p>
+          Você Acertou
+          {' '}
+          {results.reduce((somatorioAtual, resultAtual) => {
+            const isTrue = resultAtual === true;
+            if (isTrue) {
+              return somatorioAtual + 1;
+            }
+
+            return somatorioAtual;
+          }, 0)}
+          {' '}
+          Pergunta(s)
+        </p>
+        <ul>
+          {results.map((result, resultIndex) => (
+            <li key={`result_${result}`}>
+              {`${resultIndex + 1}º questão: ${result === true ? 'Acertou!' : 'Errou!'}`}
+            </li>
+          ))}
+        </ul>
+      </Widget.Content>
+    </Widget>
+  );
+}
 
 function LoadingWidget() {
   return (
@@ -29,8 +63,14 @@ function QuestionWigdet({
   question,
   totalQuestions,
   questionIndex,
+  onSubmit,
+  addResult,
 }) {
+  const [selectedAlternative, setSelectedAlternative] = React.useState(undefined);
+  const [isQuestionSubmited, setIsQuestionSubmited] = React.useState(false);
   const questionId = `question__${questionIndex}`;
+  const iscorrect = selectedAlternative === question.answer;
+  const hasAlternativeSelected = selectedAlternative !== undefined;
 
   return (
     <Widget>
@@ -51,46 +91,111 @@ function QuestionWigdet({
       <Widget.Content>
         <h2>{question.title}</h2>
         <p>{question.description}</p>
-        <form>
+        <AlternativesForm
+          onSubmit={(event) => {
+            event.preventDefault();
+            setIsQuestionSubmited(true);
+
+            setTimeout(() => {
+              addResult(iscorrect);
+              onSubmit();
+              setIsQuestionSubmited(false);
+              setSelectedAlternative(undefined);
+            }, 2 * 1000);
+          }}
+        >
           {question.alternatives.map((alternative, alternativeIndex) => {
             const alternativeId = `alternative__${alternativeIndex}`;
+            const alternativeStatus = iscorrect ? 'SUCCESS' : 'ERROR';
+            const isSelected = selectedAlternative === alternativeIndex;
+
             return (
               <Widget.Topic
                 as="label"
+                key={alternativeId}
                 htmlFor={alternativeId}
+                data-selected={isSelected}
+                data-status={isQuestionSubmited && alternativeStatus}
               >
                 <input
+                  style={{ display: 'none' }}
                   id={alternativeId}
                   name={questionId}
                   type="radio"
+                  onChange={() => setSelectedAlternative(alternativeIndex)}
                 />
                 {alternative}
               </Widget.Topic>
             );
           })}
-        </form>
-        <Button>Container</Button>
+          <Button
+            type="submit"
+            disabled={!hasAlternativeSelected}
+          >
+            próxima pergunta
+          </Button>
+          {isQuestionSubmited && iscorrect && <p>Resposta Correta!</p>}
+          {isQuestionSubmited && !iscorrect && <p>Resposta Errada!</p>}
+        </AlternativesForm>
       </Widget.Content>
     </Widget>
   );
 }
 
+const screenStates = {
+  QUIZ: 'QUIZ',
+  LOADING: 'LOADING',
+  RESULT: 'RESULT',
+};
+
 export default function QuizPage() {
+  const [screenState, setScreenState] = React.useState(screenStates.LOADING);
+  const [results, setResults] = React.useState([]);
   const totalQuestions = db.questions.length;
-  const questionIndex = 0;
+  const [questionIndex, setQuestionIndex] = React.useState(0);
   const question = db.questions[questionIndex];
+
+  function addResult(result) {
+    setResults([
+      ...results,
+      result,
+    ]);
+  }
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1 * 1000);
+  }, []);
+
+  function handleSubmit() {
+    const nextQuestion = questionIndex + 1;
+
+    if (nextQuestion < totalQuestions) {
+      setQuestionIndex(nextQuestion);
+    } else {
+      setScreenState(screenStates.RESULT);
+    }
+  }
 
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <QuizLogo />
-        <QuestionWigdet
-          question={question}
-          questionIndex={questionIndex}
-          totalQuestions={totalQuestions}
-        />
-        <Footer />
-        <LoadingWidget />
+
+        {screenState === screenStates.QUIZ && (
+          <QuestionWigdet
+            question={question}
+            questionIndex={questionIndex}
+            totalQuestions={totalQuestions}
+            onSubmit={handleSubmit}
+            addResult={addResult}
+          />
+        )}
+
+        {screenState === screenStates.LOADING && <LoadingWidget />}
+
+        {screenState === screenStates.RESULT && <ResultWidget results={results} />}
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/ricardohclandim/alura-quiz-rhcl" />
     </QuizBackground>
